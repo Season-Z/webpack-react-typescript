c5fa7f87a6a96ec6d9af427cae09fb4f73b427ba
zhouxishun <1050494995@qq.com>
2020-11-26 22:39:21 +0800

    feat:调整组件

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,22 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+// import App from './App'
+
+interface IProps {
+  name: string
+  age: number
+}
+
+const App = (props: IProps) => {
+  return (
+    <div className="app">
+      <span className="hhh">{`Hello! I'm ${props.name}, ${props.age} years old.`}</span>
+    </div>
+  )
+}
 
 if (module && module.hot) {
   module.hot.accept()
 }
 
 ReactDOM.render(<App name="vortesnail" age={25} />, document.querySelector('#root'))
-
-// 6127f7936e941b59b5515480fb4b23505edad50f
